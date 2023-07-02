abc0314f0ea3731327f3679c04eca4a68b54923f
matheus-sousa007 <mvsousa5@gmail.com>
2023-07-02 05:06:34 -0300

    WIP: Update Queries

--- bd/consultas/novaconsulta5.sql
+++ bd/consultas/novaconsulta5.sql
@@ -1,6 +1,10 @@
-SELECT DISTINCT COUNT(DISTINCT (TCAA.titulo, TCAA.artista))/2 AS ParesTagsAlbumArtista, COUNT(DISTINCT (TCAC.titulo, TCAC.cliente))/2 AS ParesTagsAlbumCliente
-FROM TagClassificaAlbumArtista AS TCAA
-CROSS JOIN TagClassificaAlbumCliente AS TCAC
-JOIN Tag AS T
-ON TCAA.tag = T.nome OR TCAC.tag = T.nome
-GROUP BY (TCAA.artista, TCAC.cliente);
+SELECT Nome, ParesTagsAlbumArtista, ParesTagsAlbumCliente
+FROM (
+	SELECT DISTINCT T.nome AS Nome, COUNT(DISTINCT (TCAA.titulo, TCAA.artista))/2 AS ParesTagsAlbumArtista, COUNT(DISTINCT (TCAC.titulo, TCAC.cliente))/2 AS ParesTagsAlbumCliente
+	FROM TagClassificaAlbumArtista AS TCAA
+	CROSS JOIN TagClassificaAlbumCliente AS TCAC
+	JOIN Tag AS T
+	ON TCAA.tag = T.nome OR TCAC.tag = T.nome
+	GROUP BY (T.nome, TCAA.artista, TCAC.cliente)
+) AS F
+WHERE ParesTagsAlbumArtista > 1 OR ParesTagsAlbumCliente > 1;
